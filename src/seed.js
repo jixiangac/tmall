a6feb59a8b3aa92429602e41ef6fa34f459055db
jixiangac <jixiangac@gmail.com>
2014-06-24 10:54:23 +0800

    g

--- src/seed.js
+++ src/seed.js
@@ -1,8 +1,12 @@
+var src = './src';
+if (location.href.indexOf('github')!==-1){
+	src = '/tmall/src';
+}
 KISSY.config({
     packages: {
         "tmall-f2e":{
             tag: "20130618",
-            base: "/tmall/src",
+            base: src,
             charset: "utf-8",
             ignorePackageNameInUri:true,
             debug: true
